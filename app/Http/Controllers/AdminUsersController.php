9f83ec298c29cb37286fa30af09ab50b4e565db6
edwardlorilla <edwardlorilla123@gmail.com>
2016-06-02 01:01:47 +0800

    Laravel 5.2edit updating user and displaying errors blade

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserCreateRequest;
+use App\Http\Requests\UserUpdateRequest;
 use App\Photo;
 use App\Role;
 use App\User;
@@ -95,9 +96,20 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UserUpdateRequest $request, $id)
     {
         //
+        $user = User::findOrFail($id);
+        $input = $request->all();
+        if($file = $request->file('photo_id')) {
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['file' => $name]);
+            $input['photo_id'] = $photo->id;
+        }
+        $user->update($input);
+
+        return redirect('/admin/users');
 
     }
 
