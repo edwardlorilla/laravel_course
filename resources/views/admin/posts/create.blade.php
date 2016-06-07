e248dcdde427b6aab950cab71020f86aca7b7ffd
edwardlorilla <edwardlorilla123@gmail.com>
2016-06-08 01:29:52 +0800

    Laravel 5.2 create post setup

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,28 +1,36 @@
 @extends('layout')
 @section('content')
     <h1>Create Post</h1>
-    {!! Form::open(['method'=>'POST', 'action'=>'AdminPostsController@store']) !!}
-    <div class="form-group">
-        {!! Form::label('title', 'Title:') !!}
-        {!! Form::text('title', null, ['class'=>'form-control']) !!}
+    {!! Form::open(['method'=>'POST', 'action'=>'AdminPostsController@store','files'=>true]) !!}
+    <div class="row">
+        <div class="form-group">
+            {!! Form::label('title', 'Title:') !!}
+            {!! Form::text('title', null, ['class'=>'form-control']) !!}
 
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('category_id', 'Category:') !!}
+            {!! Form::select('category_id', ['' =>'Choose an option'],null, ['class'=>'form-control']) !!}
+
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('photo_id', 'Photo:') !!}
+            {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
+
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('body', 'Description:') !!}
+            {!! Form::textarea('body',null, ['class'=>'form-control', 'rows'=>3]) !!}
+        </div>
+
+        <div class="form-group">
+        {!! Form::submit('Create Post', ['class'=>'form-control']) !!}
+        </div>
     </div>
-
-    <div class="form-group">
-        {!! Form::label('category_id', 'Category:') !!}
-        {!! Form::select('category_id', ['' =>'Choose an option'],null, ['class'=>'form-control']) !!}
-
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('photo_id', 'Photo:') !!}
-        {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
-
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('body', 'Description:') !!}
-        {!! Form::textarea('body',null, ['class'=>'form-control', 'rows'=>3]) !!}
-
+    <div class="row">
+        @include('includes.form_error')
     </div>
     @endsection
